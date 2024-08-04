29ed08c34696bcfa52190782348624984dcc5ecb
shreyasrivastava11 <112487189+shreyasrivastava11@users.noreply.github.com>
2024-08-04 23:19:53 +0530

    final

---
+++ currency/src/customHooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://v6.exchangerate-api.com/v6/02ad9df61336b9147b194895/latest/${currency}`
+    )
+      .then((res) => res.json)
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
