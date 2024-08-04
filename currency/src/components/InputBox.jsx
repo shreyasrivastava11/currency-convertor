df62805604c266d13306a0332f9edc5bbf2fda9c
shreyasrivastava11 <112487189+shreyasrivastava11@users.noreply.github.com>
2024-08-05 01:07:12 +0530

    final

--- currency/src/components/InputBox.jsx
+++ currency/src/components/InputBox.jsx
@@ -1,6 +1,15 @@
-// import React from 'react';
+import React from 'react';
 
-function InputBox({label, amount, onAmountChange, onCurrencyChange, currencyOption=[],selectCurrency="usd",amountDisable= false,currencyDisable=false,className="",}) {
+function InputBox({
+    label, 
+    amount, 
+    onAmountChange, 
+    onCurrencyChange, 
+    currencyOption=[],
+    selectCurrency="usd",
+    amountDisable= false,
+    currencyDisable=false,
+    className="",}) {
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
